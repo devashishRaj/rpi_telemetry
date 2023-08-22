ffc1804777affaa44731124644c952f0a01bf2c6
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2023-08-22 18:18:15 +0530

    added temperture alert module

---
+++ server/postgresDB/tempAlert.go
@@ -0,0 +1,31 @@
+package postgresDB
+
+import (
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func AlertTemp() {
+	// Set custom prefix and flags to differentiate alerts
+	log.SetPrefix("[ALERT] ")
+	//The log.SetFlags function is used to configure the log message format to include the date,
+	//time, and source file information.
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	query := `
+		SELECT AVG(temperature) AS avg_temperature
+		FROM telemetry.rpib
+		WHERE timestamp >= NOW() - INTERVAL '60 seconds';
+	`
+	var avgTemperature float64
+	err := db.QueryRow(query).Scan(&avgTemperature)
+	if err != nil {
+		fmt.Println("Query error")
+	}
+	CheckError(err)
+	if avgTemperature > 44.5 {
+		log.Printf("Average temperature within the last 30 seconds of %s : %.2f\n", jsonData.HardwareID, avgTemperature)
+
+	}
+}
